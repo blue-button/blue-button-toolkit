692d3d16937c14000b71cb6182d493687dd1c21c
jedwood <jed@limechile.com>
2014-10-23 06:51:47 -0600

    dynamic display of share data

--- _src/js/bbtoolkit.js
+++ _src/js/bbtoolkit.js
@@ -9,11 +9,33 @@ $(function() {
     $(this).addClass('active');
     evt.preventDefault();
     showResources();
+    filterShareType($(this));
   });
 
   var showResources = function() {
     var $sharedetails = $('#sharedetails');
     if (!($sharedetails.hasClass('in'))) $sharedetails.addClass('in');
+  }
+
+  var filterShareType = function($el) {
+    $('#sharedetails .share-name').text($el.data('share-name'))
+    var $ccdaListItems = $('#sharedetails .ccda-list-item').not('.keeper');
+    $ccdaListItems.addClass('hide');
+
+    var options = ['view-type','download-type','transmit-structure','transmit-vocabulary'];
+    for (var i = 0; i<options.length; i++) {
+      var leString = $el.data(options[i])
+      if (leString != ''){
+        updateSection(options[i], leString.split(","));
+      }
+    }
+
+    function updateSection(dataType,vals) {
+      for (var i=0; i<vals.length; i++) {
+        var valReady = vals[i].replace(/(\.|\-|\s|\/)/g, '');
+        $('#sharedetails .ccda-list-item[data-'+dataType+'="'+valReady+'"]').removeClass('hide');
+      }
+    }
   }
 
   FastClick.attach(document.body);
